0f3b8b616a9ca308a7484fe6361827f81ea464c7
Ivan Repilado <ivanrepi@hotmail.com>
2021-12-20 10:34:02 +0100

    added SQL_query to the lab

---
+++ module-1/mysql/SQLQuery_1.sql
@@ -0,0 +1,3 @@
+SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+FROM SalesLT.ProductCategory pc JOIN SalesLT.Product p ON pc.productcategoryid = p.productcategoryid
+ORDER BY CategoryName
